f477a8fcc2627a2bb496513997fb8dcdb3453134
timerunner16 <calvin@koose.com>
2025-03-31 09:28:19 -0400

    bufferline styling

--- lua/bufferline_conf.lua
+++ lua/bufferline_conf.lua
@@ -1,7 +1,25 @@
 local bl = require("bufferline")
 bl.setup({
+	highlights = {
+		indicator_selected = {
+			fg = "#d65d0e",
+			bg = "#282828",
+		},
+		indicator_visible = {
+			fg = "#d65d0e",
+			bg = "#282828",
+		},
+	},
 	options = {
 		mode = "tabs",
-		separator_style = "slant"
+		max_name_length = 24,
+		tab_size = 24,
+		diagnostics = "coc",
+		diagnostics_update_in_insert = true,
+		indicator = {
+			icon = "█",
+		},
+		separator_style = "thick",
+		enforce_regular_tabs = true,
 	}
 })
