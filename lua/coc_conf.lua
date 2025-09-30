f79401635dcfdaa36bbba33bb30c8ca2f1043470
timerunner16 <calvin@koose.com>
2025-09-30 10:52:32 -0400

    added code action keybinds to coc

--- lua/coc_conf.lua
+++ lua/coc_conf.lua
@@ -20,8 +20,29 @@ function _G.check_back_space()
 	return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
 end
 
-local opts = {silent=true,noremap=true,expr=true}
-vim.keymap.set('i', "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
-vim.keymap.set('i', "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
-vim.keymap.set("n", "<leader>qf", "<plug>(coc-fix-current)", opts)
-vim.keymap.set("n", "<leader>em", "<plug>(coc-codeaction-cursor)", opts)
+function _G.show_docs()
+	local cw = vim.fn.expand('<cword>')
+	if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
+		vim.api.nvim_command('h ' .. cw)
+	elseif vim.api.nvim_eval('coc#rpc#ready()') then
+		vim.fn.CocActionAsync('doHover')
+	else
+		vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+	end
+end
+
+local opts_gen = {silent=true, noremap = true, expr = true}
+local opts_ca = {silent=true}
+
+vim.keymap.set('i', "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts_gen)
+vim.keymap.set('i', "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts_gen)
+vim.keymap.set("n", "<leader>qf", "<Plug>(coc-fix-current)", opts_ca)
+vim.keymap.set("n", "<leader>em", "<Plug>(coc-codeaction-cursor)", opts_ca)
+vim.keymap.set("n", "<leader>rn", "<Plug>(coc-rename)", opts_ca)
+vim.keymap.set("n", "[g", "<Plug>(coc-diagnostic-prev)", opts_ca)
+vim.keymap.set("n", "]g", "<Plug>(coc-diagnostic-next)", opts_ca)
+vim.keymap.set("n", "<leader>cd", "<Plug>(coc-definition)", opts_ca)
+vim.keymap.set("n", "<leader>ctd", "<Plug>(coc-type-definition)", opts_ca)
+vim.keymap.set("n", "<leader>ci", "<Plug>(coc-implementation)", opts_ca)
+vim.keymap.set("n", "<leader>cr", "<Plug>(coc-references)", opts_ca)
+vim.keymap.set("n", "<leader>sd", "<Cmd>lua _G.show_docs()<CR>", opts_ca)
